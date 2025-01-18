813ba5208db58fe5946d23c05ca86bfd22289bbc
Abdullah Arafat <62858251+kmaba@users.noreply.github.com>
2025-01-18 04:50:01 +0000

    >

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 			<div className="text-center">
 				<h1 className="text-6xl font-black">...less</h1>
 				<button className="mt-4 px-6 py-2 bg-white bg-opacity-30 backdrop-blur-md text-black font-medium rounded transition ease-in-out duration-300">
-					go pendless ->
+					go pendless -`{'>'}`
 				</button>
 			</div>
 		</div>
